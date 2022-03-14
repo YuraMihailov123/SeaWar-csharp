031a8766852de8142ce6882457ec829dc1e83278
Polliakov <anton.pollyakov@gmail.com>
2022-03-15 00:44:50 +0300

    Тупа флекс...

---
+++ Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace SeaWars
+{
+    class Player
+    {
+        public Player(int[,] enemyMap)
+        {
+            this.enemyMap = enemyMap;
+        }
+
+        private int[,] enemyMap;
+
+        public bool Shoot(Button target)
+        {
+            bool hit;
+            var position = (Point)target.Tag;
+
+            if (enemyMap[position.Y, position.X] != 0)
+            {
+                hit = true;
+                enemyMap[position.Y, position.X] = 0;
+                target.BackColor = Color.Blue;
+                target.Text = "X";
+            }
+            else
+            {
+                hit = false;
+                target.BackColor = Color.Black;
+            }
+
+            return hit;
+        }
+    }
+}
